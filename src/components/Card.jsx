0338a6b20ea03be56ff018a34ac2c05da4a2c114
caroline <carololiveiratenorio21@gmail.com>
2025-03-07 16:47:16 +0000

    click

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,30 @@
+import { useEffect } from "react";
 import { FaTrashCan } from "react-icons/fa6";
 
 export default function Card (){
 
-  
+    const [dadosCard, setDadosCard] = useState ("")
+
+  useEffect( ()=>{
+    fetch ("http://localhost:3000/dados", {
+        method: "GET",
+        headers:{
+            "Content-type": "application/json",
+        },
+  })
+  .then((resposta) => resposta.json())
+  .then((data) => {
+    setDadosCard(data)
+
+  })
+  .catch((err) => console.log(err))
+},[])
 
 
     return(
         <div className="flex justify-center">
         <div className="w-[250px] bg-slate-100 font-semibold border-[1px] h-[250px] border-blue-400 p-[20px]">
-            <p>Nome:</p>
+            <p>Nome:{dadosCard}</p>
             <p>Número Inicial:</p>
             <p>Número Limite:</p>  
         <div className="flex justify-end mt-[120px]">
