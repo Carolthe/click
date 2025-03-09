65ad03d4738d189b482e2f5280c318fae0c7737a
caroline <carololiveiratenorio21@gmail.com>
2025-03-09 20:58:12 +0000

    click

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,9 @@
 import { useEffect, useState } from "react";
-import { FaTrashCan } from "react-icons/fa6";
-import Button from "../components/Button";
-import { useNavigate } from 'react-router-dom';
+import CardDados from "./CardDados";
 
 export default function Card (){
 
-    const navigate = useNavigate();
-
-    const navigatePag2 = () => {
-       navigate('/pag2');
-     };
+   
 
     const [dadosCard, setDadosCard] = useState ([])
 
@@ -31,20 +25,15 @@ export default function Card (){
     
 
     return(
-        <div className="flex flex-col justify-center">
-        <div className="w-[250px] bg-slate-100 font-semibold border-[1px] h-[250px] border-blue-400 p-[20px] overflow-auto">
+       <div className="grid justify-center">
+        <div className="flex flex-wrap mx-[127px]">
         {dadosCard.map((item, index) => (
-            <div key={index}>
-                <p>Nome:<span className="text-green-600"> {item.nomePessoa} </span></p>
-                <p>Número Inicial:<span className="text-green-600"> {item.numeroInicial}</span></p>
-                <p>Número Limite:<span className="text-green-600">{item.numeroLimite}</span> </p>
-            </div>
+        <div key={index}>
+        <CardDados item={item}/>
+        </div>
         ))}
-        <div className="flex justify-end pt-[90px]">
-        <button  ><FaTrashCan /></button> 
         </div>
-        </div>
-        <Button className="bg-blue-500 w-[300px] text-[23px] text-white mt-[30px] h-[60px]" text="Incrementar valor" onClick={navigatePag2} />
+       
         </div>
     )
 }
---
+++ src/components/CardDados.jsx
@@ -0,0 +1,26 @@
+import { FaTrashCan } from "react-icons/fa6";
+import Button from "../components/Button";
+import { useNavigate } from 'react-router-dom';
+
+export default function CardDados (props){
+
+     const navigate = useNavigate();
+    
+        const navigatePag2 = () => {
+           navigate('/pag2');
+         };
+
+    return(
+        <div className="flex justify-center "   >
+                <div className="w-[250px] bg-slate-100 font-semibold border-[1px] h-[250px] border-blue-400 p-[20px] overflow-auto">
+                        <p>Nome:<span className="text-green-600"> {props.item.nomePessoa} </span></p>
+                        <p>Número Inicial:<span className="text-green-600"> {props.item.numeroInicial}</span></p>
+                        <p>Número Limite:<span className="text-green-600">{props.item.numeroLimite}</span> </p>
+                <div className="flex justify-between pt-[90px]">
+                <Button className="bg-blue-500 w-[130px] text-[14px] text-white h-[25px]" text="Alterar número" onClick={navigatePag2} />
+                <button  ><FaTrashCan /></button> 
+                </div>
+            </div>
+        </div>
+    )
+}
